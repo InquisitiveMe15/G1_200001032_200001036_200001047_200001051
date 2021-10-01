a64fb940e5e292814ea6cbadd448d2679cc55180
InquisitiveMe15 <85685489+InquisitiveMe15@users.noreply.github.com>
2021-10-01 22:12:36 +0530

    final app.py

---
+++ question4_5/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
+from pandas import DataFrame
+import re
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/getfile", methods=['POST', 'GET'])
+def getfile():
+    if request.method == 'POST':
+        textfile = request.files["textfile"]
+        csvfile = request.files["csvfile"]
+        csvfilename = secure_filename(csvfile.filename)
+        textfilename = secure_filename(textfile.filename)
+
+        textfile.save(os.path.join(textfilename))
+        csvfile.save(os.path.join(csvfilename))
+
+        f = open("attendance.txt", 'r')
+        arrayOfRollNos = []
+        arrayOfNames = []
+        # proxyStudents = []
+
+        df = pd.read_csv('reference.csv')
+        df['Attendance'] = 0
+
+        col = ['S.No.', 'RollNo.', 'Name', 'Attendance']
+        proxydf = pd.DataFrame(columns=col)
+
+        first = 1
+        nameInAttendance=""
+        rollno=""
+        while True:
+            line1 = f.readline()
+            line2 = f.readline()
+            if not  line1:
+                break
+            arrayOfNames.append(re.split('\d+',line1)[0])
+            nameInAttendance=arrayOfNames[-1].split(" ")[0]
+            if first == 1:
+                nameInAttendance=nameInAttendance[3:]
+                first = first + 1
+            arrayOfRollNos.append(line2.split(" ")[0])
+            rollno=arrayOfRollNos[-1]
+            
+            present = False
+            for j in list(df['RollNo.']):
+                if j == int(rollno):
+                    present = True
+            if present == True:
+
+                name = df[df['RollNo.'] == int(rollno)]['Name'].values[0] 
+                SNo = df[df['RollNo.'] == int(rollno)]['S.No.'].values[0] 
+                
+                
+                proxyNAME = []   
+                if bool(nameInAttendance.upper()==str(name.split(" ")[0])):
+                    if df[df['RollNo.'] == int(rollno)]['Attendance'].values[0] != -10:
+                        df.iloc[SNo-1,-1]=1
+                    
+                else:
+                    proxyName = ""
+                    flag = False
+                    for i in list((df['Name'])):
+                        if str(i).split(" ")[0] == str(nameInAttendance.upper()):
+                            proxyName = i
+                            proxyNAME.append(i)
+                            flag = True
+
+                    if flag == True:
+                        proxyRollNo = df[df['Name'] == str(proxyName)]['RollNo.'].values[0]
+                        SNo1 = df[df['RollNo.'] == int(proxyRollNo)]['S.No.'].values[0]
+                        df.iloc[SNo1-1,-1]=-10
+
+        
+        proxydf = df[df['Attendance']==-10]
+        filtereddf = proxydf
+        return render_template("filteredtable.html",filtereddf = filtereddf)
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
